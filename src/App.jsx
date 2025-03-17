2972c75b05b9cf628231f263ca75286ad059104a
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-17 12:32:09 +0300

    build search component with auto options

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import "./App.css"
-import React from 'react'
-import Search from "./Components/Search/Search"
+import "./App.css";
+import React from "react";
+import Search from "./Components/Search/Search";
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+
   return (
     <div className="container">
-      <Search />
-      
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/api.jsx
@@ -0,0 +1,18 @@
+const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "4292b97589msh272335f08c512c6p1bfc9cjsn23387316916f",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
